fc6f398ade484662a1b4d853d77eadb303072767
masaki-i02 <otiken2000@gmain.com>
2026-09-02 00:11:14 +0900

    初期コミット

---
+++ src/backend/kintai/src/test/java/jp/co/sample/kintai/KintaiApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.sample.kintai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KintaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
